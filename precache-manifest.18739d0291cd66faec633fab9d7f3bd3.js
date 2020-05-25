f612cd6c0d59f62aeb50a0e44ff8f0d35ba4db16
Diana <diana_ivanivna@kr.net>
2020-05-25 16:04:55 +0300

    Updates

---
+++ precache-manifest.18739d0291cd66faec633fab9d7f3bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9536c34fbdb64324387c08ddc73f621e",
+    "url": "/test-task-paragraphs/index.html"
+  },
+  {
+    "revision": "d7e7b26ca2c3d5c842de",
+    "url": "/test-task-paragraphs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a34dffcfd97b184de5de",
+    "url": "/test-task-paragraphs/static/js/2.e74c7ed3.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/test-task-paragraphs/static/js/2.e74c7ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e7b26ca2c3d5c842de",
+    "url": "/test-task-paragraphs/static/js/main.a94657d5.chunk.js"
+  },
+  {
+    "revision": "deda0a36243de00501ae",
+    "url": "/test-task-paragraphs/static/js/runtime-main.1dfde986.js"
+  }
+]);
